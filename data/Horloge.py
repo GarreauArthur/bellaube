3d9110b7f2d3dfc1314960440b4b52357b0025ea
Garreau Arthur <arthurgarreau.web@gmail.com>
2018-04-18 01:13:49 +0200

    Une classe Horloge pour gérer l'heure facilement

---
+++ data/Horloge.py
@@ -0,0 +1,43 @@
+from time import sleep
+from Heure import Heure
+
+class Horloge:
+	"""
+	Cette classe va permettre de gérer l'écoulement du temps
+
+	ex utilisation :
+	h = Horloge();
+	while 1 :
+		print(h.now())
+		h.tictac()
+		sleep(1)
+	"""
+	def __init__(self):
+		self.heure = Heure(0,0);
+
+	def now(self):
+		return self.heure.__str__()
+
+	def tictac(self):
+		#sleep(1)
+		self.setSecondes(self.heure.getSecondes()+1)
+	
+	def setSecondes(self,s):
+		if(s == 60):
+			self.heure.setSecondes(0)
+			self.setMinutes(self.heure.getMinutes()+1)
+		else:
+			self.heure.setSecondes(s)
+
+	def setMinutes(self,m):
+		if(m == 60):
+			self.heure.setMinutes(0)
+			self.setHeures(self.heure.getHeures()+1)
+		else:
+			self.heure.setMinutes(m)
+
+	def setHeures(self,h):
+		if(h == 24):
+			self.heure.setHeures(0)
+		else:
+			self.heure.setHeures(h)
