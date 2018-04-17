295ee73efd8574ac9f0eec14064d2aa7ee864b44
Garreau Arthur <arthurgarreau.web@gmail.com>
2018-04-18 01:13:15 +0200

    Une classe Heure pour représenter les heures

---
+++ data/Heure.py
@@ -0,0 +1,45 @@
+import datetime
+
+class Heure:
+	"""
+	Cette classe permet de gérer la représentation de l'heure
+	que ce soit pour l'heure "actuelle" ou pour les alarmes
+	"""
+
+	def __init__(self,h=0,m=0):
+		"""
+		Créer un objet heure initialisé à h:m
+		h pour heures
+		m pour minutes
+		"""
+		self.heure = datetime.time(h,m)
+
+	def __str__(self):
+		return self.heure.__str__()
+
+	def setHeures(self,h):
+		"""
+		Permet de modifier les heures
+		"""
+		self.heure = self.heure.replace(h,self.heure.minute)
+
+	def setMinutes(self,m):
+		"""
+		Permet de modifier les minutes
+		"""
+		self.heure = self.heure.replace(self.heure.hour,m)
+
+	def setSecondes(self,s):
+		"""
+		Permet de modifier les secondes
+		"""
+		self.heure = self.heure.replace(self.heure.hour,self.heure.minute,s)
+
+	def getHeures(self):
+		return self.heure.hour
+
+	def getMinutes(self):
+		return self.heure.minute
+
+	def getSecondes(self):
+		return self.heure.second
