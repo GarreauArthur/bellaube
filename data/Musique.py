89a68ab8e89ac5b645136fe70f919ee4453a2128
BIZEUL Emmanuel <bizeulem@master-17-18>
2018-04-23 15:30:15 +0200

    Classe Musique et Main

---
+++ data/Musique.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+class Musique:
+
+	def __ini__(self):
+		self.liste = [] 
+
+	def getListe(self):
+		return self.liste
+
+	def setListe(self,l):
+		self.liste = l
+
+	'''
+	def ajouterSon(self,son):
+		self.liste.append(son)
+	''' 
---
+++ main/Main.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+from time import sleep
+from Heure import Heure
+from Alarme import Alarme
+from Horloge import Horloge
+from Reglages import Reglages
+
+def main():
+    '''Variables globales'''
+    
+    horloge = Horloge()
+    alarme = Alarme()
+    musique = Musique()
+    
+    
+    
+if __name__== '__main__':
+    main() 
+
+
