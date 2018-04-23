8a8286d37b7a40a07de9257e742b30b0f3e34ba6
GiletBenoit <38322679+GiletBenoit@users.noreply.github.com>
2018-04-23 16:32:58 +0200

    Update EcranLCD.py

--- ecran/EcranLCD.py
+++ ecran/EcranLCD.py
@@ -1,7 +1,20 @@
 #-*- coding: utf-8 -*-
 
+# Imports
+from RPLCD.i2c import CharLCD
+
 class EcranLCD:
     """
     Cette classe permet de simplifier l'utilisation des écrans LCD
     """
-
+    def __init__(self):
+        self.lcd = CharLCD('PCF8574',0x27)
+        self.lcd.clear()
+        
+    def printString(self, char):
+        if(isinstance(char,str)):
+            self.lcd.clear()
+            self.lcd.write_string(char)
+    
+    def clear(self):
+        self.lcd.clear()
