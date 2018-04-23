44666ca5de42c577770dc7df29b104f6441dab1c
BIZEUL Emmanuel <bizeulem@master-17-18>
2018-04-23 16:14:23 +0200

    Commentaire main

--- main/Main.py
+++ main/Main.py
@@ -4,13 +4,28 @@ from Heure import Heure
 from Alarme import Alarme
 from Horloge import Horloge
 from Reglages import Reglages
+import RPi.GPIO as GPIO
 
 def main():
-    '''Variables globales'''
+    '''Instanciation Variables globales'''
     
     horloge = Horloge()
     alarme = Alarme()
     musique = Musique()
+
+    '''Sauvegarde des données'''
+
+    '''Initialisation'''
+	
+    '''Cœur du programme : 
+    	while(1):
+		gestion des threads
+        	mise à jour horloge
+		heure horloge == heure alarme ?
+			déclencher réveil
+ 
+    '''
+
     
     
     
