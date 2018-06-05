fa5e13eeb57987b93b262267e59409f6256ef649
Arthur Garreau <arthurgarreau.web@gmail.com>
2018-06-05 19:10:40 +0200

    Transformation du tableau menus en chaine de caracteres

--- main/menus_proto.py
+++ main/menus_proto.py
@@ -26,10 +26,21 @@ GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)#GPIO22
 
 def Valider(channel):
    global profondeur
-   if profondeur < 4 :
-      profondeur += 1
+   ancetres_str = "".join(str(a) for a in menus[:profondeur])
+   
+   if ancetres_str == "00":
+   elif ancetres_str == "011":
+   elif ancetres_str == "0120":
+   elif ancetres_str == "0121":
+   elif ancetres_str == "0131":
+   elif ancetres_str == "0132":
+   elif ancetres_str == "12":
+   elif ancetres_str == "21":
    else:
-      profondeur = 0
+      if profondeur < 4 :
+         profondeur += 1
+         menu_str = ancetres_str + str(menus[profondeur])
+         print(MENUS_AFFICHAGE[menu_str])
 
 GPIO.add_event_detect(15, GPIO.FALLING, callback=Valider, bouncetime=300)
 
@@ -57,7 +68,24 @@ while(1):
    elif intervalle > 0 : # equivalent à un else tout seul
       menus[profondeur] += 1;
    compteur_prec = compteur
-   print(menus)
+   
+   ancetres = menus[:profondeur] # on récupère les ancetres
+   ancetres_str = "".join(str(e) for e in ancetres) # on les convertit en cc
+   nb_sous_menus = TOUS_LES_NOMBRES_DE_SOUS_MENUS[ancetres_str] # on récupère le nb menus
+   if nb_sous_menus == 101 : # si une echelle
+      if menus[profondeur] < 0 :
+         menus[profondeur] = 0
+      elif menus[profondeur] > 100 :
+         menus[profonder] = 100
+      print(menus[profondeur])
+   elif nb_sous_menus == 1440 : # si une heure
+      heures = val//60
+      minutes = val % 60 
+      print(heures + ":" + minutes)
+   else: # si menu normal
+      menus[profondeur] = menus[profondeur]%nb_sous_menus
+      menus_str = "".join(str(a) for a in menus[:profondeur+1])
+      print(MENUS_AFFICHAGE[menus_str])
 
 
 
