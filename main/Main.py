ee144ad32c934bda8776ed097a5a0a6b7ac9c8c4
Garreau Arthur <arthurgarreau.web@gmail.com>
2018-06-09 11:49:24 +0200

    début init

--- main/Main.py
+++ main/Main.py
@@ -4,15 +4,33 @@ from ..data.Heure import Heure
 from ..data.Alarme import Alarme
 from ..data.Horloge import Horloge
 from ..data.Reglages import Reglages
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from ..data.Musique import Musique
+from ..data.ConstantePin import *
+
+
+
+#---------------------------------INITIALISATION--------------------------------
+
+
+# set mode des GPIOs
+GPIO.setmode(IO.BOARD)
+
+# Instanciation Variables globales
+horloge = Horloge()
+alarme = Alarme()
+musique = Musique()
+reglages = Reglages()
+reglages.addAlarme(alarme)
+# gestion des interruptions
+#     menus, valider
+#     retour
+#     eteindre alarme
+
+#--------------------------------BOUCLE-PRINCIPALE------------------------------
 
 def main():
     '''Instanciation Variables globales'''
-    
-    horloge = Horloge()
-    alarme = Alarme()
-    musique = Musique()
 
     '''Sauvegarde des données'''
 
